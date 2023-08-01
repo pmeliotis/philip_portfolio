3962dc78fda7dbe6161eb0d1fbde2b9fd73fb3ee
pmeliotis <134556164+pmeliotis@users.noreply.github.com>
2023-08-01 23:22:26 +0200

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,28 +1,18 @@
-import { Html, useProgress } from "@react-three/drei";
-
+import { useProgress, Html } from "@react-three/drei";
 const CanvasLoader = () => {
   const { progress } = useProgress();
   return (
-    <Html
-      as='div'
-      center
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-      }}
-    >
-      <span className='canvas-loader'></span>
+    <Html>
+      <span className="canvas-load"></span>
       <p
         style={{
           fontSize: 14,
-          color: "#F1F1F1",
+          color: "#f1f1f1",
           fontWeight: 800,
           marginTop: 40,
         }}
       >
-        {progress.toFixed(2)}%
+        {progress.toFixed(0)}%
       </p>
     </Html>
   );
