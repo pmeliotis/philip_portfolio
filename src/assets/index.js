3e151b45a791cb5fbdbc49b277719f18b680eeb6
pmeliotis <philip.meliotis@gmail.com>
2023-08-01 19:36:57 +0200

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,40 @@
+import logo from "./logo.png";
+import web from "./web.png";
+import github from "./github.png";
+import linkedin from "./linkedin.svg";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+import threejs from "./tech/threejs.svg";
+
+import eydap from "./company/eydap.png"
+
+import renthome from "./renthome.png";
+
+export {
+  logo,
+  web,
+  github,
+  linkedin,
+  menu,
+  close,
+  css,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  tailwind,
+  threejs,
+  eydap,
+  renthome,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,124 @@
+import {
+    web,
+    linkedin,
+    javascript,
+    html,
+    css,
+    reactjs,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    eydap,
+    renthome,
+    threejs,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+
+ 
+  
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    }
+  ];
+  
+  const experiences = [
+    {
+      title: "It support engineer(Internship)",
+      company_name: "Eydap",
+      icon: eydap,
+      iconBg: "#383E56",
+      date: "11/2021 – 04/2022",
+      points: [
+        "Install and configure computer hardware, software, networks and applications.",
+        "Respond to technical support calls from other staff members or clients and communicate how to resolve issues.",
+        "Frequent communication with the developers for software troubleshooting."
+      ],
+    },
+  ];
+  
+  
+
+  
+  const projects = [
+    {
+      name: "rent Home",
+      description:
+        "Web-based platform that allows users to search, rent, and buy a home in United Arab Emirates.",
+      tags: [
+        {
+          name: "javascript",
+          color: "orange-text-gradient",
+        },
+        {
+          name: "reactjs",
+          color: "blue-text-gradient",
+        },
+        {
+        name: "nextjs",
+          color: "green-text-gradient",
+        }
+      ],
+      image: renthome,
+      source_code_link: "https://github.com/pmeliotis/real_estate_project",
+    },
+  ];
+  
+  export {  services, technologies, experiences,  projects };
